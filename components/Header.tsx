eed84c0c1e76453856af04c9d18943bf25352067
Cemre Kur <cemrekur@gmail.com>
2023-08-03 21:46:17 +0300

    project init

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import { mainNavLinks, socials } from "@/lib/data";
+function Header() {
+	return (
+		<header>
+			<div className="container"></div>
+		</header>
+	);
+}
+
+export default Header;
---
+++ lib/data.ts
@@ -0,0 +1,41 @@
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+
+export const mainNavLinks = [
+	{
+		name: "Home",
+		hash: "#home",
+	},
+	{
+		name: "About",
+		hash: "#about",
+	},
+	{
+		name: "Projects",
+		hash: "#projects",
+	},
+	{
+		name: "Skills",
+		hash: "#skills",
+	},
+	{
+		name: "Experience",
+		hash: "#experience",
+	},
+	{
+		name: "Contact",
+		hash: "#contact",
+	},
+] as const;
+
+export const socials = [
+	{
+		label: "Visit Github Page Of Cemre",
+		icon: AiFillGithub,
+		href: "https://github.com/cemsjam",
+	},
+	{
+		label: "Visit LinkedIn Page Of Cemre",
+		icon: AiFillLinkedin,
+		href: "https://github.com/cemsjam",
+	},
+] as const;
